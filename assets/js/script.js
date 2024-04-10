3eceb8cd5f9340346659d346c362d18ede9b0a64
Mithun Yadav <mithundesigner01@gmail.com>
2024-04-10 12:59:53 +0530

    feat: home page complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,17 @@ const bottomFooter = document.querySelector("footer");
 const mainSection = document.querySelector("main");
 
 
-
+let homeAllSection = document.querySelectorAll(".homePage");
 
 
 topHeader.addEventListener("click",(e)=>{
     let tabName = e.target.innerText;
     tabName == "NEWS" ? document.documentElement.style.setProperty("--tabChangeAnime", "50%") : document.documentElement.style.setProperty("--tabChangeAnime", "0");
+
+    homeAllSection.forEach((item)=>{
+        item.style.display="none";
+    });
+
+    let homeSelectedSection = document.querySelector(`#${tabName.toLowerCase()}-section`);
+    homeSelectedSection.style.display="flex";
 });
